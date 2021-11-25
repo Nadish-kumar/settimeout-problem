39b7d1e70f2c3bfed1481cfae0e193a0215dd709
Nadish-kumar <92292340+Nadish-kumar@users.noreply.github.com>
2021-11-25 20:40:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(function(){
+    document.getElementById("count").innerText = "10";
+    setTimeout(function(){
+        document.getElementById("count").innerText = "9"
+        setTimeout(function(){
+            document.getElementById("count").innerText = "8"
+            setTimeout(function(){
+                document.getElementById("count").innerText = "7"
+                setTimeout(function(){
+                    document.getElementById("count").innerText = "6"
+                    setTimeout(function(){
+                        document.getElementById("count").innerText = "5"
+                        setTimeout(function(){
+                            document.getElementById("count").innerText = "4"
+                            setTimeout(function(){
+                                document.getElementById("count").innerText = "3"
+                                setTimeout(function(){
+                                    document.getElementById("count").innerText = "2"
+                                    setTimeout(function(){
+                                        document.getElementById("count").innerText = "1"
+                                        setTimeout(function(){
+                                            document.getElementById("count").innerText = " Happy Independence Day "
+                                            
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
